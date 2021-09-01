b8e0f1415903e7a49c61236c6cbffb24486129f0
leandrobarr <leandrobarr@yahoo.com.br>
2021-09-01 14:04:36 -0300

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+        /*int a = 28;
+        int b = 50;
+        System.out.println("Hello World! " + (a + b));*/
+    }
+}
